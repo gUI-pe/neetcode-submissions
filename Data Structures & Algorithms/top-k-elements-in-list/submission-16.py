b4833a964c77d3a033bee64b7a1132452ecfdb6e
Guilherme Peruci <121200588+gUI-pe@users.noreply.github.com>
2026-08-19 16:30:34 +0200

    Add: top-k-elements-in-list - submission-16

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-16.py
@@ -0,0 +1,22 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # 1. Count frequencies
+        d = defaultdict(int)
+        for n in nums:
+            d[n] += 1
+
+        # 2. Map frequency -> list of numbers with that frequency
+        d_inverse = defaultdict(list)
+        for n, count in d.items():
+            d_inverse[count].append(n)  # Append to avoid overwriting
+
+        ordered_freqs = sorted(d_inverse.keys(), reverse=True)
+        
+        res = []
+
+        for freq in ordered_freqs:
+            res.extend(d_inverse[freq])
+            if len(res) >= k:
+                return res
+
+        return res
