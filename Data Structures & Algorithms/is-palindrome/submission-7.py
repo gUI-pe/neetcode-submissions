c4883cfc91431a72463507885849b7c1cfd53d26
Guilherme Peruci <121200588+gUI-pe@users.noreply.github.com>
2026-08-31 01:10:14 +0200

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # Pre-process the string (strip non-alphanumeric and lowercase)
+        filtered = [c.lower() for c in s if c.isalnum()]
+        
+        n = len(filtered)
+        for i in range(n // 2):
+            if filtered[i] != filtered[n - 1 - i]:
+                return False
+        return True
